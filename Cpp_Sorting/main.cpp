bced04721363cdf963242bfde0bb3b84ecc59d67
georgebzhang <43728180+georgebzhang@users.noreply.github.com>
2019-02-20 20:42:29 -0800

    selection sort array and std vector

--- Cpp_Sorting/main.cpp
+++ Cpp_Sorting/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <array>
 #include <vector>
 
-void print_arr(int* arr, int size) {
+void print_arr(const int* const arr, const int size) {
 	for (int i = 0; i < size; ++i) {
 		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
 }
 
-void print_vec(std::vector<int>& vec) {
+template<typename T, size_t N>
+void print_arr_std(const std::array<T, N>& const arr_std) {
+	for (int i = 0; i < N; ++i) { // N or arr_std.size()
+		std::cout << arr_std[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void print_vec(const std::vector<int>& const vec) {
 	for (int i = 0; i < vec.size(); ++i) {
 		std::cout << vec[i] << " ";
 	}
@@ -21,7 +30,7 @@ void swap(int& a, int& b) {
 	b = temp;
 }
 
-int find_ind_min_arr(int* arr, int size) {
+int find_ind_min_arr(const int* const arr, const int size) {
 	int ind_min = 0;
 	int val_min = arr[0];
 	for (int i = 0; i < size; ++i) {
@@ -33,19 +42,34 @@ int find_ind_min_arr(int* arr, int size) {
 	return ind_min;
 }
 
-int find_ind_min_vec(std::vector<int>& vec) {
+//template<typename T, size_t N>
+//int find_ind_min_arr_std(const std::array<T, N>& const arr_std) {
+//	int ind_min = 0;
+//	int val_min = arr_std[0];
+//	for (int i = 0; i < N; ++i) {
+//		if (arr_std[i] < val_min) {
+//			val_min = arr_std[i];
+//			ind_min = i;
+//		}
+//	}
+//	return ind_min;
+//}
+
+int find_ind_min_vec(const std::vector<int>::iterator vec_begin, const std::vector<int>::iterator vec_end) {
 	int ind_min = 0;
-	int val_min = vec[0];
-	for (int i = 0; i < vec.size(); ++i) {
-		if (vec[i] < val_min) {
-			val_min = vec[i];
+	int val_min = *vec_begin;
+	int i = 0;
+	for (std::vector<int>::iterator it = vec_begin; it != vec_end; ++it) {
+		if (*it < val_min) {
+			val_min = *it;
 			ind_min = i;
 		}
+		++i;
 	}
 	return ind_min;
 }
 
-void selection_sort_arr(int* arr, int size) {
+void selection_sort_arr(int* arr, const int size) {
 	int ind_min;
 	for (int i = 0; i < size; ++i) {
 		ind_min = find_ind_min_arr(&arr[i], size - i) + i; // (&arr[i], size - i) means we are passing a subarray of arr from indices i -> size
@@ -56,24 +80,26 @@ void selection_sort_arr(int* arr, int size) {
 void selection_sort_vec(std::vector<int>& vec) {
 	int ind_min;
 	for (int i = 0; i < vec.size(); ++i) {
-		std::vector<int> sub_vec = vec
-		ind_min = find_ind_min_vec(vec)
+		ind_min = find_ind_min_vec(vec.begin() + i, vec.end()) + i;
+		swap(vec[i], vec[ind_min]);
 	}
 }
 
 int main() {
 	int test_arr[] = { 5, 1, 7, 2, 9, 3, 6 };
 	int size = sizeof(test_arr) / sizeof(test_arr[0]);
-
+	//std::array<int, 7> test_arr_std{ 5, 1, 7, 2, 9, 3, 6 };
 	std::vector<int> test_vec{ 5, 1, 7, 2, 9, 3, 6 };
+
+	print_arr(test_arr, size);
+	//print_arr_std(test_arr_std);
+	print_vec(test_vec);
+
+	selection_sort_arr(test_arr, size);
+	selection_sort_vec(test_vec);
 
 	print_arr(test_arr, size);
 	print_vec(test_vec);
 
-	selection_sort_arr(test_arr, size);
-
-
-	print_arr(test_arr, size);
-
-	while (1);
+	std::cin.get();
 }
